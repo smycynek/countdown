9a49dee2609d18f33c44215371c1859a94f46950
Steven Mycynek <smycynek@buildingengines.com>
2020-06-26 20:32:54 -0400

    use hooks

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
-import Countdown from './Countdown'
+import Countdown from './Countdown';
 import './App.css';
 
-  class App extends React.Component {
-    
-    render() {
-      return (
-        <div className="App">
-          <header className="App-header">
-          <Countdown 
-          target_caption="until 'No Time to Die' is released in theaters/streaming."
-          target_date={new Date(2020,10,20,6,0,0,0)}/>
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Countdown
+            targetCaption="until 'No Time to Die' is released in theaters/streaming."
+            targetDate={new Date(2020, 10, 20, 6, 0, 0, 0)}
+          />
 
-          <div className="partition">
+          <div className="partition" />
 
-          </div>
+          <Countdown
+            targetCaption="until KFMDM comes to Boston!"
+            targetDate={new Date(2021, 3, 14, 20, 0, 0, 0)}
+          />
 
-          <Countdown 
-          target_caption="until KFMDM comes to Boston!"
-          target_date={new Date(2021,3,14,20,0,0,0)}/> 
-          </header>
-        </div>
-      );
-    }
-  };
+          <div className="partition" />
+
+          <Countdown />
+        </header>
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/Countdown.js
+++ src/Countdown.js
@@ -1,90 +1,90 @@
-import React from 'react';
+/* eslint-disable react/jsx-filename-extension */
+import React, { useState, useEffect } from 'react';
+import { PropTypes } from 'prop-types';
 import './App.css';
 
-  /*
+/*
   Simple component to animate a countdown in
   hours, minutes, seconds from a given date
   */
-  class Countdown extends React.Component {
+const Countdown = ({ targetDate, targetCaption }) => {
 
-    getTimeSpan(date) {
-      return date.getTime() - new Date().getTime();
-    }
+  const getTimeSpan = (date) => date.getTime() - new Date().getTime();
 
-    getSpanComponents(span) {
-      var days = span/(1000*60*60*24);
-      var days_whole = Math.floor(days);
-      var remainder_hours = days - days_whole;
-      var hours = remainder_hours*24
-      var hours_whole = Math.floor(hours);
-      var remainder_minutes = hours - hours_whole;
-      var minutes = remainder_minutes*60;
-      var minutes_whole = Math.floor(minutes);
-      var remainder_seconds = minutes - minutes_whole;
-      var seconds = remainder_seconds*60;
-      var seconds_whole = Math.floor(seconds);
-      return {
-        days: days_whole,
-        hours: hours_whole,
-        minutes: minutes_whole,
-        seconds: seconds_whole,
-      };
-    }
-    
-    getSpanFormatted(date) {
-      var span = this.getTimeSpan(date)
-      var spanComponents = this.getSpanComponents(span);
-      var day_label = "days";
-      if (spanComponents.days === 1) {
-        day_label = "day";
-      }
-      var hour_label = "hours";
-      if (spanComponents.hours === 1) {
-        hour_label = "hour";
-      }
-      var minute_label = "minutes";
-      if (spanComponents.minutes === 1) {
-        minute_label = "minute";
-      }
-      var seconds_label = "seconds";
-      if (spanComponents.seconds === 1) {
-        seconds_label = "second";
-      }
-      return `${spanComponents.days} ${day_label}, 
-      ${spanComponents.hours} ${hour_label}, ${spanComponents.minutes} 
-      ${minute_label}, ${spanComponents.seconds} ${seconds_label}`;
-      }
-
-    constructor (props) {
-      super(props)
-      this.state = {  interval: 500,
-      spanFormatted: this.getSpanFormatted(props.target_date),
-      };
-    }
-
-    componentDidMount () {
-      this.timerID = setInterval(
-        () => this.tick(),
-        500
-      )
-    }
-  
-    tick() {
-      this.setState({spanFormatted: this.getSpanFormatted(this.props.target_date)})
-    }
-
-    componentWillUnmount () {
-      clearInterval(this.timerID)
-    }
-
-    render() {
-      return (
-        <div>
-          <h1>{this.state.spanFormatted}</h1>
-          <h2>{this.props.target_caption}</h2>
-        </div>
-      );
-    }
+  const getSpanComponents = (span) => {
+    const days = span / (1000 * 60 * 60 * 24);
+    const daysWhole = Math.floor(days);
+    const remainderHours = days - daysWhole;
+    const hours = remainderHours * 24;
+    const hoursWhole = Math.floor(hours);
+    const remainderMinutes = hours - hoursWhole;
+    const minutes = remainderMinutes * 60;
+    const minutesWhole = Math.floor(minutes);
+    const remainderSeconds = minutes - minutesWhole;
+    const seconds = remainderSeconds * 60;
+    const secondsWhole = Math.floor(seconds);
+    return {
+      days: daysWhole,
+      hours: hoursWhole,
+      minutes: minutesWhole,
+      seconds: secondsWhole,
+    };
   };
+
+  const createSpanFormatted = (date) => {
+    const span = getTimeSpan(date);
+    const spanComponents = getSpanComponents(span);
+    let dayLabel = 'days';
+    if (spanComponents.days === 1) {
+      dayLabel = 'day';
+    }
+    let hourLabel = 'hours';
+    if (spanComponents.hours === 1) {
+      hourLabel = 'hour';
+    }
+    let minuteLabel = 'minutes';
+    if (spanComponents.minutes === 1) {
+      minuteLabel = 'minute';
+    }
+    let secondLabel = 'seconds';
+    if (spanComponents.seconds === 1) {
+      secondLabel = 'second';
+    }
+    return `${spanComponents.days} ${dayLabel}, 
+      ${spanComponents.hours} ${hourLabel}, ${spanComponents.minutes} 
+      ${minuteLabel}, ${spanComponents.seconds} ${secondLabel}`;
+  };
+
+  const [spanFormatted, setSpanFormatted] = useState(
+    createSpanFormatted(targetDate),
+  );
+
+  const tick = () => {
+    setSpanFormatted(createSpanFormatted(targetDate));
+  };
+
+  useEffect(() => {
+    const timerID = setInterval(() => tick(), 500);
+    return () => {
+      clearInterval(timerID);
+    };
+  }, []);
+
+  return (
+    <div>
+      <h1>{spanFormatted}</h1>
+      <h2>{targetCaption}</h2>
+    </div>
+  );
+};
+
+Countdown.propTypes = {
+  targetDate: PropTypes.instanceOf(Date),
+  targetCaption: PropTypes.string,
+};
+Countdown.defaultProps = {
+  targetCaption: 'Until June 1, 2030, 9 p.m.',
+  targetDate: new Date(2030, 5, 1, 21, 0, 0, 0),
+};
 
 export default Countdown;
